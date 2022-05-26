60d3c1c563f1192d25d2fba5cbb25775101ba7c8
danielcr <daniel.correa.1995@gmail.com>
2022-05-26 18:33:05 -0500

    created header to chose hotel date and people

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+
+function Home() {
+  return (
+      <>
+        <Navbar/>
+        <Header/>
+      </>
+  )
+}
+
+export default Home
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+
+
+function List() {
+  return (
+    <>
+        <Navbar/>
+        <Header type = "list"/>
+    </>
+  )
+}
+
+export default List
